957e03063a9a41920e16ed6f555f21d0e8d50f02
Mark Pinho <mark.pinho@outlook.com>
2024-07-08 23:36:49 -0400

    Forgot to upload 04-SumeEveryExercise.js in previous commit

---
+++ 03-JavaScript/09-Callback&ArrayMethods/CodingExercises/04-SomeEveryExercise.js
@@ -0,0 +1,6 @@
+const allEvens = (numbers) => numbers.every(num => num % 2 === 0);
+
+// if you want to define this as a traditional Function
+// function allEvens(numbers) {
+//     return numbers.every(num => num % 2 === 0);
+// }git
