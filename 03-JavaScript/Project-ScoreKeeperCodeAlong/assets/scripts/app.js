c5244c7ae33ab89bb114365c1be972995cfad19b
Mark Pinho <mark.pinho@outlook.com>
2024-07-16 09:57:11 -0400

    Cleaning up the order of the functions for readability, all functionality remains the same

--- 03-JavaScript/Project-ScoreKeeperCodeAlong/assets/scripts/app.js
+++ 03-JavaScript/Project-ScoreKeeperCodeAlong/assets/scripts/app.js
@@ -20,6 +20,14 @@ const selectScore = document.querySelector("#selectScore");
 let winningScore = 2;
 let gameOver = false;
 
+// Actions that happen when the buttons are clicked
+const scoreReset = () => {
+    gameOver = false;
+    [ p1.score, p1.display.innerHTML, p2.score, p2.display.innerHTML ] = [ 0, 0, 0, 0 ];
+    p1.display.classList.remove("has-text-success", "has-text-danger");
+    p2.display.classList.remove("has-text-success", "has-text-danger");
+}
+
 const update = (player, opponent) => {
     if (!gameOver) {
         if (player.score < winningScore) {
@@ -36,22 +44,12 @@ const update = (player, opponent) => {
     }
 }
 
-
+// Functionality of buttons
 p1.button.addEventListener('click', () => update(p1, p2));
-
 p2.button.addEventListener('click', () => update(p2, p1));
-
-const scoreReset = () => {
-    gameOver = false;
-    [ p1.score, p1.display.innerHTML, p2.score, p2.display.innerHTML ] = [ 0, 0, 0, 0 ];
-    p1.display.classList.remove("has-text-success", "has-text-danger");
-    p2.display.classList.remove("has-text-success", "has-text-danger");
-}
-
 reset.addEventListener('click', scoreReset);
 
 selectScore.addEventListener('change', () => {
     winningScore = Math.ceil(parseInt(selectScore.value) / 2);
     scoreReset();
 });
-
