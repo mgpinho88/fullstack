cf4295db542a180f9cbec8319b853e6502ead9f6
Mark Pinho <mark.pinho@outlook.com>
2024-08-23 16:31:04 -0400

    Finish the FarmShop project

--- 04-NodeAndExpress/express04-Express&Mongoose/index.js
+++ 04-NodeAndExpress/express04-Express&Mongoose/index.js
@@ -17,13 +17,13 @@ const port = 3000
 // Mongoose Connection
 const applicationDB = "farmShop"
 mongoose.connect(`mongodb://127.0.0.1:27017/${applicationDB}`)
-    .then(() => {
-        console.log(`Mongo connection open to: ${applicationDB}`)
-    })
-    .catch(err => {
-        console.log(`Could not connect to ${applicationDB}`)
-        console.log(err)
-    })
+  .then(() => {
+    console.log(`Mongo connection open to: ${applicationDB}`)
+  })
+  .catch(err => {
+    console.log(`Could not connect to ${applicationDB}`)
+    console.log(err)
+  })
 
 // This will point to our directory for "views" and setup our project to use EJS
 app.set('views', path.join(__dirname, 'views'));
@@ -38,44 +38,52 @@ app.listen(port, () => {
 
 // Test route to ensure application is running
 app.get('/test', async (req, res) => {
-    res.render('test');
+  res.render('test');
 });
 
 // Render main page (Read route)
 app.get('/shop', async (req, res) => {
-    const products = await Product.find({});
-    res.render('shop/index', { products });
+  const products = await Product.find({});
+  res.render('shop/index', { products });
 });
 
 // Create Routes
 app.get('/shop/new', (req, res) => {
-    res.render('shop/newItem', { Categories })
+  res.render('shop/newItem', { Categories })
 })
 
 app.post('/shop', async (req, res) => {
-    const product = new Product(req.body);
-    await product.save();
-    res.redirect('/shop');
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect('/shop');
 })
 
 // Read Routes
 app.get('/shop/:id', async (req, res) => {
-    console.log("Line36:GET ", req.body)
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render('shop/item', { product });
+  console.log("Line36:GET ", req.body)
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('shop/item', { product });
 })
 
 // Update Routes
 app.get('/shop/:id/edit', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render('shop/editItem', { product, Categories});
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('shop/editItem', { product, Categories});
 })
 
 app.put('/shop/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true})
-    console.log(product)
-    res.redirect('/shop')
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true})
+  console.log(product)
+  res.redirect('/shop')
+})
+
+// Delete Route
+app.delete('/shop/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndDelete(id);
+  console.log(`${product} has been deleted from DB`);
+  res.redirect('/shop');
 })
